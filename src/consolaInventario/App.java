eba91e779f0b867c77a45001598a26f11d23a925
danielrealesp <85652354+danielrealesp@users.noreply.github.com>
2021-10-16 12:04:07 -0500

    Carga CSV en Menu

--- src/consolaInventario/App.java
+++ src/consolaInventario/App.java
@@ -1,10 +1,66 @@
 package consolaInventario;
+import java.util.*;
 
 public class App {
 
 	public static void main(String[] args) {
-		//Iniciar la aplicación
 		System.out.println("Aplicación Iniciada");
+		
+		App app = new App();
+		app.desplegarMenu();
 	}
+	
+	public void desplegarMenu()
+	{
+		//Despliega el Menú Principal de la Aplicación de Inventario
+		boolean continuar = true;
+
+		while (continuar)
+		{
+
+			System.out.println("1. Cargar Información CSV");
+			System.out.println("2. Salir de la Aplicación");
+
+			int respuesta = Integer.parseInt(input("Seleccione su opción"));
+
+			if (respuesta == 2)
+			{
+				continuar = false;
+				System.out.println("Hasta luego");
+			}
+			else
+			{
+				ejecutar(respuesta);
+			}
+		}
+	}
+	
+	private String input(String mensaje)
+	{
+			Scanner input = new Scanner(System.in);
+			System.out.printf("%s", mensaje + ": ");
+			String respuesta = input.nextLine();
+			return respuesta;
+		
+	}
+	
+	private void ejecutar(int respuesta)
+	{
+		switch (respuesta)
+		{
+			case 1:
+				cargarCSV();
+				break;
+		}
+	}
+	
+	
+	private void cargarCSV()
+	{
+		String ruta = input("Ingrese la ruta de su archivo");
+		System.out.printf("%s\n", ruta);
+	}
+	
+	
 	
 }
