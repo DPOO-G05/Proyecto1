d39c68cc83fc2bfbda2d42f10719c8455eb5abe2
danielrealesp <85652354+danielrealesp@users.noreply.github.com>
2021-10-17 13:28:42 -0500

    Avances Carga Inventario

--- src/procesamientoInventario/LectorProducto.java
+++ src/procesamientoInventario/LectorProducto.java
@@ -22,25 +22,47 @@ public class LectorProducto {
 		
 		for (ArrayList<String> linea : data)
 		{
-			String id = linea.get(2);
-			String nombre = linea.get(3);
-			String categoria = linea.get(4);
-			String gondola = linea.get(5);
-			String marca = linea.get(6);
-			String empacadoStr = linea.get(7);
+			String id = linea.get(2).strip();
+			String nombre = linea.get(3).strip().toUpperCase();
+			String categoria = linea.get(4).strip().toUpperCase();
+			String gondola = linea.get(5).strip().toUpperCase();
+			String marca = linea.get(6).strip().toUpperCase();
+			String empacadoStr = linea.get(7).toUpperCase();
 			int unidades = Integer.parseInt(linea.get(8));
 			double costo = Double.parseDouble(linea.get(9));
 			double precioVenta = Double.parseDouble(linea.get(10));
-			String pesoNeto = linea.get(11);
-			String unidadMedida = linea.get(12); 
+			String pesoNeto = linea.get(11).strip().toUpperCase();
+			String unidadMedida = linea.get(12).strip().toUpperCase(); 
 
 			
 			String idLote = linea.get(0);
 			Lote lote = lotes.get(idLote);
 			Producto product = productos.get(id);
 
+			//Asignar Nombre
+			product.setNombre(nombre);
+			
+			//Asignar Marca 
+			product.setMarca(marca);
+			
+			//Asignar empacado
+			if(empacadoStr.equals("N"))
+			{
+				product.setEmpacado(false);
+			}
+			else
+			{
+				product.setEmpacado(true);
+			}
+			
+			//Incrementar Cantidad
+			
+			
+			
+			
 			if (categoria.equals("CONGELADO"))
 			{
+				ProductoCongelado congelado = (ProductoCongelado) product;
 				
 			}
 			
